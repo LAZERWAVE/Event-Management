66bd8984fab76d26fae219bb9321e6b784d05550
jethro <jethrootto@gmail.com>
2026-06-07 21:09:48 +0700

    Create hero section for landing page

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+import ExploreBtn from "@/components/ExploreBtn";
+
+const Page = () => {
+
+    console.log("Im from here");
+
+    return (
+        <section className="text-center">
+            <h1>The Hub for Event Management</h1>
+            <p>Hackathons, Meetups, and Conferences, All In One Place</p>
+
+            <ExploreBtn />
+        </section>
+
+    )
+}
+export default Page
